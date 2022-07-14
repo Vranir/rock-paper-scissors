a733c7726bf222342bcdb7b6fa36e5960d537c34
Vladimir Bralic <vlado.bralic@gmail.com>
2022-07-14 14:57:54 +0200

    Gui branch inital commit

--- code.js
+++ code.js
@@ -6,16 +6,94 @@ function compPlay(){
 }
 
 function compare(cmp, plyr ){
-    if (cmp=="rock"&&plyr=="scissors") return "You lose! Rock beats scissors!"
-    else if (cmp=="scissors"&&plyr=="paper") return "You lose! Scissors beat paper!"
-    else if (cmp=="paper"&&plyr=="rock") return "You lose! Paper beats rock!"
-    else if (cmp=="scissors"&&plyr=="rock") return "You win! Rock beats scissors!"
-    else if (cmp=="paper"&&plyr=="scissors") return "You win! Scissors beat paper!"
-    else if (cmp=="rock"&&plyr=="paper") return "You win! Paper beats rock!"
-    else return "Draw!";  
+    if (cmp=="rock"&&plyr=="scissors") return 0
+    else if (cmp=="scissors"&&plyr=="paper") return 0
+    else if (cmp=="paper"&&plyr=="rock") return 0
+    else if (cmp=="scissors"&&plyr=="rock") return 1
+    else if (cmp=="paper"&&plyr=="scissors") return 1
+    else if (cmp=="rock"&&plyr=="paper") return 1
+    else return 3;  
 }
 
-function game(){
+function restart(){
+    let btn = document.createElement("button");
+    btn.innerHTML = "Play Again?";    
+    btn.style.alignSelf="center";
+    btn.style.padding="20px 80px";
+    document.body.appendChild(btn);
+    btn.addEventListener('click',()=>{
+        btn.parentNode.removeChild(btn);
+        btns.forEach((btn) => {btn.disabled=false}) 
+    cs=0
+    ps=0
+
+    document.getElementById('cscore').textContent="Computer: "+cs;        
+    document.getElementById('pscore').textContent="Player: "+ps;
+    
+    document.getElementById('winlose').setAttribute('style','font-size:18px');
+    document.getElementById('winlose').setAttribute('style','font-weight:normal');
+    document.getElementById('winlose').textContent="...";
+    });
+
+}
+
+let wl="";
+let cs=0;
+let ps=0;
+
+const btns=document.querySelectorAll('button');
+
+btns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        let cc=compPlay();
+        let result=compare(cc,btn.id);
+        if (result==0){
+            wl="You Lose!"
+            cs+=1
+        }
+        else if (result==1){
+            wl="You win!"
+            ps+=1
+        }
+        else{
+            wl="Draw!"
+        }
+        let msg="Computer played "+cc+"! You played "+btn.id+"! "+wl;
+            
+        document.getElementById('winlose').textContent=msg;
+        document.getElementById('cscore').textContent="Computer: "+cs;        
+        document.getElementById('pscore').textContent="Player: "+ps;
+        
+
+        if (cs==3){
+            document.getElementById('winlose').textContent="Computer victory!";
+            document.getElementById('winlose').setAttribute('style','font-size:32px');
+            document.getElementById('winlose').setAttribute('style','font-weight:bold');
+            btns.forEach((btn) => {btn.disabled=true})
+            restart()
+        }
+        else if (ps==3){
+            document.getElementById('winlose').textContent="Player victory!";
+            document.getElementById('winlose').setAttribute('style','font-size:32px');
+            document.getElementById('winlose').setAttribute('style','font-weight:bold');
+            btns.forEach((btn) => {btn.disabled=true})
+            restart()
+        }
+    
+    
+            
+    });
+});
+
+
+
+
+
+function play(e){
+    console.log(this);
+}
+
+/*function game(){
     for (let i=0; i<5; i++){
         let plyrSl= prompt("Your Choice?").toLowerCase();
         let cmpSl=compPlay();
@@ -25,6 +103,6 @@ function game(){
         console.log("\n")
 
     }
-}
+}*/
 
-game();
+//game();
